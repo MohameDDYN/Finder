fbaee9c5978fb1fde93aaa6b3e3080e5c5a3da89
deyenem <mohameddeyene28@gmail.com>
2026-07-18 12:46:49 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Finder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
